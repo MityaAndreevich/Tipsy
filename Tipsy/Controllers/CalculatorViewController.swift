2c952e66c2df366928efccde3c0eba062a15c45d
MityaAndreevich <dmitry.logachev@icloud.com>
2021-10-25 15:14:13 +0300

    selection of buttons and print added

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -12,8 +12,8 @@ class CalculatorViewController: UIViewController {
     
     //MARK: - IBOutlets
     @IBOutlet weak var billTextField: UITextField!
-    @IBOutlet weak var zeroPctButton: UIButton!
     
+    @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     
@@ -21,12 +21,32 @@ class CalculatorViewController: UIViewController {
     
     //MARK: - IBActions
     @IBAction func tipChanged(_ sender: UIButton) {
+        if sender == zeroPctButton {
+            zeroPctButton.isSelected = true
+            tenPctButton.isSelected = false
+            twentyPctButton.isSelected = false
+        } else if sender == tenPctButton {
+            zeroPctButton.isSelected = false
+            tenPctButton.isSelected = true
+            twentyPctButton.isSelected = false
+        } else {
+            zeroPctButton.isSelected = false
+            tenPctButton.isSelected = false
+            twentyPctButton.isSelected = true
+        }
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        if zeroPctButton.isSelected == true {
+            print("0.0")
+        } else if tenPctButton.isSelected == true {
+            print("0.1")
+        } else {
+            print("0.2")
+        }
     }
     
 }
